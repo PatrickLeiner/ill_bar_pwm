7913593baf45f20383c7fcb27509507aaedc9ba1
PatrickLeiner <p4tr1ck.l31n3r@gmail.com>
2016-01-20 18:23:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -21,6 +21,7 @@
 #include <ti/sysbios/knl/Task.h>
 #include <ti/sysbios/knl/Semaphore.h>
 #include <ti/sysbios/knl/Mailbox.h>
+#include <ti/sysbios/hal/Timer.h>
 
 #include <driverlib/udma.h>
 #include <driverlib/i2c.h>
@@ -36,11 +37,44 @@
 
 /* Example/Board Header files */
 #include "Board.h"
-#include "ill_bar_click.h"
+
+/*
+ *  ======== Illuminatino_click commands ========
+ */
+#define SLAVEADDR                   0x49
+#define CONTROL                     0x80
+#define CLEAR                       0x70
+#define TIMING                      0x01
+#define THRESHLOWLOW                0x02
+#define THRESHLOWHIGH               0x03
+#define THRESHHIGHLOW               0x04
+#define THRESHHIGHHIGH              0x05
+#define INTERRUPT                   0x06
+#define CRC                         0x08
+#define ID                          0x0A
+#define DATA0LOW                    0x0C
+#define DATA0HIGH                   0x0D
+#define DATA1LOW                    0x0E
+#define DATA1HIGH                   0x0F
+#define PWR_ON                      0x03
+#define PWR_OFF                     0x00
+#define READWORD0_COMMAND           0xAC
+#define READWORD1_COMMAND           0xAE
+#define LOW_GAIN_INTERVAL_402MS     0x02    //Gain(x1)
+#define LOW_GAIN INTERVAL_101MS     0x01    //Gain(x1)
+#define HI_GAIN_INTERVAL_101MS      0x11    //Gain(x16)
+
+/*
+ *  ======== defines ========
+ */
+#define LIGHT_GAIN  20
+
 
 void task_ill();
 void task_bar();
-void pwm_fxn();
+void pwm_on();
+void pwm_off();
+void timer_set();
 int value_conversion(uint32_t value);
 void init_i2c();
 void init_spi();
@@ -50,7 +84,10 @@ void mailBox_create();
 void task_create();
 void semaphore_create();
 
-
+Timer_Params ontimerParams;
+Timer_Params offtimerParams;
+Timer_Handle timerON;
+Timer_Handle timerOFF;
 Mailbox_Handle myMailbox;
 Task_Params taskParams;
 I2C_Handle i2c_handle;
@@ -79,7 +116,7 @@ int main(){
 	task_create();
 	semaphore_create();
 //	mailBox_create();
-	pwm_fxn();
+//	timer_set();
 
     BIOS_start();    /* does not return */
     return 0;
@@ -98,40 +135,40 @@ void task_ill(){
 	i2c_transac.readBuf = (uint8_t*)&readBuffer;
 	i2c_transac.writeBuf = (uint8_t*)&writeBuffer;
 
-	writeBuffer[0] = (CONTROL + PWR_ON);	//address of register to write
-//	writeBuffer[1] = PWR_ON;	//data to be written to the previous address in writeBuffer[0]
-	i2c_transac.writeCount = 1;
+	writeBuffer[0] = (CONTROL);	//address of register to write
+	writeBuffer[1] = PWR_ON;	//data to be written to the previous address in writeBuffer[0]
+	i2c_transac.writeCount = 2;
 
-		if(!I2C_transfer(i2c_handle, &i2c_transac)){
-			System_abort("Bad I2C transfer!");
-		}
-		else{
-			System_printf("Board is on\n");
-		}
-		System_flush();
+	if(!I2C_transfer(i2c_handle, &i2c_transac)){
+		System_abort("Bad I2C transfer!");
+	}
+	else{
+		System_printf("Board is on\n");
+	}
+	System_flush();
 
 	writeBuffer[0] = (CONTROL + TIMING);
 	writeBuffer[1] = LOW_GAIN_INTERVAL_402MS;
 	i2c_transac.writeCount = 2;
 
-		if(!I2C_transfer(i2c_handle, &i2c_transac)){
-			System_abort("Bad I2C transfer");
-		}
-		else{
-			System_printf("Timer set to %x\n", LOW_GAIN_INTERVAL_402MS);
-		}
-		System_flush();
+	if(!I2C_transfer(i2c_handle, &i2c_transac)){
+		System_abort("Bad I2C transfer");
+	}
+	else{
+		System_printf("Timer set to %x\n", LOW_GAIN_INTERVAL_402MS);
+	}
+	System_flush();
 
 	writeBuffer[0] = (CONTROL + INTERRUPT);
 	writeBuffer[1] = 0xF0;
 	i2c_transac.writeCount = 2;
 
-		if(!I2C_transfer(i2c_handle, &i2c_transac)){
-			System_abort("Bad I2C transfer");
-		}
-		else{
-			System_printf("Interrupt Mode\n");
-		}
+	if(!I2C_transfer(i2c_handle, &i2c_transac)){
+		System_abort("Bad I2C transfer");
+	}
+	else{
+		System_printf("Interrupt Mode\n");
+	}
 
 	/*
 			writeBuffer[0] = (CONTROL_COMMAND + REGISTER_TIMING);
@@ -146,64 +183,65 @@ void task_ill(){
 		    System_flush();
 	*/
 
-		for(;;){
-		System_printf("Running task1 function\n");
+	for(;;){
+	    System_printf("Running task1 function\n");
 
-			if (Semaphore_getCount(sem) == 0) {
-				System_printf("Sem blocked in task1\n");
-			}
-
-			/* Get access to resource */
-			Semaphore_pend(sem, BIOS_WAIT_FOREVER);
-
-			/* set i2c slave for reading */
-			writeBuffer[0] = (CONTROL + CLEAR);
-			i2c_transac.writeCount = 1;
-
-			if (!I2C_transfer(i2c_handle, &i2c_transac)) {
-				System_abort("Bad I2C transfer");
-			}
-
-			/* Address the Ch0 lower data register and configure for Read Word */
-			i2c_transac.writeCount = 1;
-			i2c_transac.readCount = 2;
-			writeBuffer[0] = READWORD0_COMMAND;
-
-			if (!I2C_transfer(i2c_handle, &i2c_transac)) {
-				System_abort("Bad I2C transfer!");
-			}
-			licht = readBuffer[1] * 256 + readBuffer[0];
-
-		    /* Address the Ch1 lower data register and configure for Read Word */
-		    i2c_transac.writeCount = 1;
-		    i2c_transac.readCount = 2;
-		    writeBuffer[0] = READWORD1_COMMAND;
-		    if (!I2C_transfer(i2c_handle, &i2c_transac)) {
-		       System_abort("Bad I2C transfer!");
-		    }
-		    irlicht = readBuffer[1] * 256 + readBuffer[0];
-
-			/* convert light and ir_light into lux */
-//			lux = CalculateLux(0,2,licht,irlicht,0);
-
-			System_printf("ILL: %u\n", licht);
-			System_printf("IR ILL: %u\n", irlicht);
-			System_printf("Lux: %u\n\n", lux);
-			System_flush();
-
-			/* do work on locked resource */
-			resource = licht;
-
-			/* unlock resource */
-	        Semaphore_post(sem);
-
-	        Task_sleep(5);
+		if (Semaphore_getCount(sem) == 0) {
+			System_printf("Sem blocked in task1\n");
 		}
+
+		/* Get access to resource */
+		Semaphore_pend(sem, BIOS_WAIT_FOREVER);
+
+		writeBuffer[0] = CONTROL;
+		writeBuffer[1] = CLEAR;
+		i2c_transac.writeCount = 2;
+
+		if (!I2C_transfer(i2c_handle, &i2c_transac)) {
+			System_abort("Bad I2C transfer");
+		}
+
+		/* Address the Ch0 lower data register and configure for Read Word */
+		i2c_transac.writeCount = 1;
+		i2c_transac.readCount = 2;
+		writeBuffer[0] = READWORD0_COMMAND;
+
+		if (!I2C_transfer(i2c_handle, &i2c_transac)) {
+			System_abort("Bad I2C transfer!");
+		}
+		licht = readBuffer[1] * 256 + readBuffer[0];
+
+		/* Address the Ch1 lower data register and configure for Read Word */
+		i2c_transac.writeCount = 1;
+		i2c_transac.readCount = 2;
+		writeBuffer[0] = READWORD1_COMMAND;
+		if (!I2C_transfer(i2c_handle, &i2c_transac)) {
+		    System_abort("Bad I2C transfer!");
+		}
+		irlicht = readBuffer[1] * 256 + readBuffer[0];
+
+		/* convert light and ir_light into lux */
+//		lux = CalculateLux(0,2,licht,irlicht,0);
+
+		System_printf("ILL: %u\n", licht);
+		System_printf("IR ILL: %u\n", irlicht);
+		System_printf("Lux: %u\n\n", lux);
+		System_flush();
+
+		/* do work on locked resource */
+		resource = licht;
+
+		/* unlock resource */
+	    Semaphore_post(sem);
+
+	    Task_sleep(5);
+	}
 }
 /*
  *  ======== task_bar ========
  */
 void task_bar(){
+    Task_sleep(10);
 	/*variables*/
     UShort msg = 0x00;
     SPI_Transaction spi_transac;
@@ -258,41 +296,14 @@ void task_bar(){
 /*
  *  ======== pwm_fxn ========
  */
-void pwm_fxn(void){
-	uint32_t duty;
-	uint32_t frequency;
-	uint32_t period;
-	uint32_t sysclock;
-
-	// Set sys clock to 120 MHz
-	sysclock = SysCtlClockFreqSet(SYSCTL_XTAL_25MHZ | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 120000000);
-
-	// Set PWM clock to 120 MHz
-	// SysCtlPWMClockSet(PWM_SYSCLK_DIV_1);
-
-	// Enable port M pin 7
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM);
-	GPIOPinTypeGPIOOutput(GPIO_PORTM_BASE, GPIO_PIN_7);
-
-	// Configure PM7 as T5CCP1
-	GPIOPinConfigure(GPIO_PM7_T5CCP1);
-	GPIOPinTypeTimer(GPIO_PORTM_BASE, GPIO_PIN_7);
-
-	// Configure Timer 3
-	frequency = 3800;
-	period = sysclock/frequency;
-	duty = period/2;
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER3);
-	TimerConfigure(TIMER3_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
-	TimerControlLevel(TIMER3_BASE, TIMER_BOTH, 0); // Trigger low
-	TimerLoadSet(TIMER3_BASE, TIMER_A, period - 1);
-	TimerLoadSet(TIMER3_BASE, TIMER_B, period - 1);
-	TimerMatchSet(TIMER3_BASE,TIMER_A, duty/8);
-	TimerMatchSet(TIMER3_BASE,TIMER_B, duty/16);
-
-	// Turn on PWM
-	TimerEnable(TIMER3_BASE, TIMER_BOTH);
-
+/*
+void pwm_on(void){
+    GPIOPinWrite(GPIO_PORTM_BASE, GPIO_PIN_7, ~(GPIOPinRead(GPIO_PORTM_BASE, GPIO_PIN_7)));
+    Timer_start(timerOFF);
+}
+*/
+void pwm_off(){
+    GPIOPinWrite(GPIO_PORTM_BASE, GPIO_PIN_7, ~(GPIOPinRead(GPIO_PORTM_BASE, GPIO_PIN_7)));
 }
 /*
  *  ======== value_check ========
@@ -457,3 +468,32 @@ void task_create(){
 void semaphore_create(){
     sem = Semaphore_create(1, NULL, NULL);
 }
+/*
+void timer_set(){
+
+    Error_Block eb;
+    Error_init(&eb);
+    Timer_Params_init(&ontimerParams);
+    Timer_Params_init(&offtimerParams);
+
+    ontimerParams.period = 10;
+    ontimerParams.periodType = Timer_PeriodType_MICROSECS;
+    ontimerParams.arg = 1;
+    ontimerParams.runMode=Timer_RunMode_CONTINUOUS;
+    ontimerParams.startMode=Timer_StartMode_AUTO;
+    timerON = Timer_create(Timer_ANY,(ti_sysbios_interfaces_ITimer_FuncPtr) pwm_on, &ontimerParams, &eb);
+    if (timerON == NULL) {
+    System_abort("Timer create failed");
+    }
+
+    offtimerParams.period = 10;
+    offtimerParams.periodType = Timer_PeriodType_MICROSECS;
+    offtimerParams.arg = 1;
+    offtimerParams.runMode=Timer_RunMode_ONESHOT;
+    offtimerParams.startMode=Timer_StartMode_USER;
+    timerOFF = Timer_create(Timer_ANY,(ti_sysbios_interfaces_ITimer_FuncPtr) pwm_off, &offtimerParams, &eb);
+    if (timerOFF == NULL) {
+    System_abort("Timer create failed");
+    }
+}
+*/
